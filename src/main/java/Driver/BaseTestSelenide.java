7eddbf92f309d9c8451b8b59365ff337ac5d6f49
Roma <nosovich1501@gmail.com>
2021-11-18 12:26:37 +0100

    added parameter env to Moodpanda test

--- src/main/java/Driver/BaseTestSelenide.java
+++ src/main/java/Driver/BaseTestSelenide.java
@@ -1,12 +1,23 @@
 package Driver;
 
+import Properties.PropertyReader;
+import TestNG.ListenerSelenide;
+import org.testng.annotations.Listeners;
+
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.page;
 import static com.codeborne.selenide.WebDriverRunner.driver;
 
+@Listeners(ListenerSelenide.class)
 public class BaseTestSelenide {
 
     public <T> T get(Class<T> pageObjectClassClass) {
         return driver().hasWebDriverStarted() ? page(pageObjectClassClass): open("https://moodpanda.com/", pageObjectClassClass);
+    }
+
+    public <PageObjectClass> PageObjectClass get(Class<PageObjectClass> pageObjectClassClass, String env) {
+        PropertyReader propertyReader = new PropertyReader();
+        propertyReader.setProperties(env);
+        return get(pageObjectClassClass);
     }
 }
